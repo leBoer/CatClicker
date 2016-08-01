e52cd5349a35cd1ee4208b1779abf3b95e73222a
leBoer <christianborre@gmail.com>
2016-08-01 20:35:29 +0800

    There are now two cats

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
 $(function(){
 
-    // Appends the image of the cat on the page
-    $('.catimage').append('<img id="clickCat" class="image" src="img/cutecat.jpg">');
+    // Manipulates the DOM and add divs for cats and counters
+    $('.cats').append('<div id="cat1" class="catdivs">', '<div id="cat2" class="catdivs">');
+
+    $('#cat1').append('<div id="counter1">');
+    $('#cat2').append('<div id="counter2">');
+
+    // Prepends the image of the cat on the page
+    $('#cat1').prepend('<img id="clickCat1" class="image" src="img/cutecat.jpg">');
+    $('#cat2').prepend('<img id="clickCat2" class="image" src="img/angrycat.jpg">');
+
+    // Prepends the names for the cats
+    $('#cat1').prepend('<p>Cute Cat</p>');
+    $('#cat2').prepend('<p>Angry Cat</p>');
 
     // Counts the number of clicks on the cat
-    var clickCounter = 0;
-    $('#clickCat').click(function(e) {
+    var clickCounter1 = 0;
+    $('#clickCat1').click(function(e) {
 
-        clickCounter++;
-        $('.counter').text("You just clicked the cat " + clickCounter + " times!");
+        clickCounter1++;
+        $('#counter1').text("You just clicked the cute cat " + clickCounter1 + " times!");
+
+    });
+
+    var clickCounter2 = 0;
+    $('#clickCat2').click(function(e) {
+
+        clickCounter2++;
+        $('#counter2').text("You just clicked the angry cat " + clickCounter2 + " times!");
 
     });
 
