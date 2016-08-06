16ee81c2e7bfb7d7666683adb496520b4de01f35
leBoer <christianborre@gmail.com>
2016-08-06 13:08:54 +0800

    Refractors to adhere with the MOV model.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 $(function(){
-    // Used to keep track of which cat we're displaying at all times
-    var q = 0;
 
-    var clickyCats = {
+    // The Model //
+    var model = {
         'cats': [
             {
                 'name': 'Cute cat',
@@ -34,10 +33,36 @@ $(function(){
                 'id': 'grumpy',
                 'counter': 0
             }
-        ],
-        // Appends the cat names to the menu
-        'display': function(){
-            clickyCats.cats.forEach(function(cat){
+        ]
+    };
+
+    // The View //
+    // The List View
+    var listview = {
+        init: function(){
+            this.render();
+
+            var catObject = octupus.getCats();
+            $('.name').click(function(e) {
+                $('#catPicture').empty();
+                $('#counterDiv').empty();
+                test = this.id;
+                for (var i = 0; i < catObject.length; i++){
+
+                    if (test == catObject[i].id){
+                        var picture = catObject[i].image;
+                        var pictureId = catObject[i].id + "Picture";
+                        $('#catPicture').append("<img src=" + picture + " id=" + pictureId + " class='image'>");
+                        $('#counterDiv').append("You have clicked " + catObject[i].name + " " + catObject[i].counter + " times");
+                        q = i;
+                    }
+                }
+
+            })
+        },
+
+        render: function(){
+            octupus.getCats().forEach(function(cat){
                 var catname = cat.name;
                 var catid = cat.id;
                 $('#catNames').append("<p id=" + catid + " class='name'>" + catname + "</br></p>");
@@ -45,41 +70,70 @@ $(function(){
         }
     };
 
-    clickyCats.display();
-
-    $('.name').click(function(e){
-        $('#catPicture').empty();
-        $('#counterDiv').empty();
-        var test = this.id;
-        // Figures out which cat we selects, and appends the picture and the correct counter to the picture
-        for (var i = 0; i < clickyCats.cats.length; i++){
-
-            if (test == clickyCats.cats[i].id){
-                var picture = clickyCats.cats[i].image;
-                var pictureId = clickyCats.cats[i].id + "Picture";
-                $('#catPicture').append("<img src=" + picture + " id=" + pictureId + " class='image'>");
-                $('#counterDiv').append("You have clicked " + clickyCats.cats[i].name + " " + clickyCats.cats[i].counter + " times");
-                q = i;
-            }
-        }
-
-    });
-
-    // Detects when we click the picture
-    $('#catPicture').click(function(e){
-        // Clears the counter before we append the new value
-        $('#counterDiv').empty();
-        // Figures out which cat it is based on the value of q which was defined in the catselector function
-        for (var t = 0; t < clickyCats.cats.length; t++){
-            if(q == t){
-
-                clickyCats.cats[t].counter++;
+    // The Picture View
+    var pictureview = {
+        init: function(){
+            this.render();
+            var catObject = octupus.getCats();
+            $('#catPicture').click(function(e){
+                // Clears the counter before we append the new value
                 $('#counterDiv').empty();
-                $('#counterDiv').append("You have clicked " + clickyCats.cats[t].name + " " + clickyCats.cats[t].counter + " times");
+                // Figures out which cat it is based on the value of q which was defined in the catselector function
+                for (var t = 0; t < catObject.length; t++){
+                    if(q == t){
 
-            }
+                        octupus.updateCounter(t);
+                        $('#counterDiv').empty();
+                        $('#counterDiv').append("You have clicked " + catObject[t].name + " " + catObject[t].counter + " times");
+
+                    }
+                }
+            });
+        },
+
+        render: function(){
+            $('#catPicture').text("Click on one of the cats to the left to display the cat")
+
         }
-    });
+    };
+
+    // The Octupus //
+    var octupus = {
+        //var q = 0;
+        getCats: function(){
+            return model.cats;
+            //return test;
+        },
+
+        updateCounter: function(t){
+            model.cats[t].counter++;
+        },
+
+        init: function(){
+            var q = 0;
+            listview.init();
+            pictureview.init();
+
+        }
+    };
+
+    octupus.init();
+
+
+    //$('#catPicture').click(function(e){
+    //    // Clears the counter before we append the new value
+    //    $('#counterDiv').empty();
+    //    // Figures out which cat it is based on the value of q which was defined in the catselector function
+    //    for (var t = 0; t < clickyCats.cats.length; t++){
+    //        if(q == t){
+    //
+    //            clickyCats.cats[t].counter++;
+    //            $('#counterDiv').empty();
+    //            $('#counterDiv').append("You have clicked " + clickyCats.cats[t].name + " " + clickyCats.cats[t].counter + " times");
+    //
+    //        }
+    //    }
+    //});
 
 
 });
